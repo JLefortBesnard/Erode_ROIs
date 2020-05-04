a235bb868c73ce8f4be0f8ade41a771869efbd1b
Jeremy Lefort-Besnard <21123003+JLefortBesnard@users.noreply.github.com>
2020-05-04 17:08:49 +0800

    Update shrink_roi.py

--- shrink_roi.py
+++ shrink_roi.py
@@ -1,35 +1,18 @@
 import nibabel as nib
-import os
-import numpy as np
-from nilearn import plotting
-import numpy as np
-
-# plotting.view_img_on_surf(img, threshold='90%', surf_mesh='fsaverage').open_in_browser() 
-
 from scipy import ndimage
-import matplotlib.pyplot as plt
-
-# plotting.plot_glass_brain(img, threshold=0.1)
-# plotting.plot_glass_brain(img_shrinked, threshold=0.1)
-# plotting.show()
-
 import glob
-for img_path in glob.glob("*relevant_*.nii"):
+
+n_ite = 2
+for img_path in glob.glob("*.nii"):
+    # load the original nifti image
     img = nib.load(img_path)
+    # extract data
     img_data = img.get_data()
-    img_data_shrinked = ndimage.binary_erosion(img_data, iterations=2).astype(img_data.dtype)     
+    # shrinked the ROI twice
+    img_data_shrinked = ndimage.binary_erosion(img_data, iterations=n_ite).astype(img_data.dtype)     
+    # build the nifti image of the new shape
     img_shrinked = nib.Nifti1Image(img_data_shrinked, img.affine, img.header)
-    new_img_name = "shrinked_imgs_2/" + img_path[:-4] + "shrinked2"
+    # save the file
+    new_img_name = img_path[:-4] + "shrink_2ite"
     nib.save(img_shrinked, new_img_name)
     
-
-# keep normal size 
-# 8, 4, 1, 11, 12, 13, 14
-# shrink 
-# 3, 5, 6, 7, 15, 2, 9, 10
-
-
-
-
-
-
